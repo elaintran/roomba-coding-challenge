420993fea4846f10b7689f5a791c5542b441d6b8
elaintran <elain.ttran@gmail.com>
2020-10-03 18:03:37 -0500

    coordinates logged without collision

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Roomba from "./pages/Roomba.js";
 import "./App.css";
 
 function App() {
   	return (
     	<div className="App">
+			<Roomba />
     	</div>
   	);
 }
--- src/pages/Roomba.js
+++ src/pages/Roomba.js
@@ -1,13 +1,60 @@
 import React, { Component } from "react";
+import roomba from "../roomba.json";
 
 class Roomba extends Component {
     state = {
+        roomDimensions: roomba[0].roomDimensions,
+        roombaLocation: roomba[0].initialRoombaLocation,
+        dirtLocation: roomba[0].dirtLocations,
+        drivingInstructions: roomba[0].drivingInstructions,
+        output: []
+    }
 
+    componentDidMount() {
+        this.getDimensions();
+    }
+
+    getDimensions = () => {
+        console.log(this.state.roombaLocation);
+        const roombaMovement = roomba[0].drivingInstructions.map(instruct => {
+            return this.getDirections(instruct);
+        });
+        console.log(roombaMovement);
+    }
+
+    getDirections = direction => {
+        let location = this.state.roombaLocation;
+        let newLocation;
+        let coordinates;
+        switch(direction) {
+            //north
+            case "N": 
+                newLocation = location[0] + 1;
+                coordinates = [newLocation, location[1]];
+                break;
+            //east
+            case "E":
+                newLocation = location[1] + 1;
+                coordinates = [location[0], newLocation];
+                break;
+            //south
+            case "S":
+                newLocation = location[0] - 1;
+                coordinates = [newLocation, location[1]];
+                break;
+            //west
+            default:
+                newLocation = location[1] - 1;
+                coordinates = [location[0], newLocation];
+        }
+        return coordinates;
     }
 
     render() {
         return (
-            <div></div>
+            <div>
+                <p></p>
+            </div>
         );
     }
 }
